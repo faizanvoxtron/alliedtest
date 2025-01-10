387581c054493fbb8bb2a7880858edfe972fbeb1
Ubuntu <ubuntu@ip-172-31-28-28.eu-north-1.compute.internal>
2025-01-10 18:49:28 +0000

    Docker File Updated

--- inc/meta.php
+++ inc/meta.php
@@ -1,15 +1,15 @@
 <?php
 
-function meta($pageTitle,$pageKeywords,$pageDescription,$pageCanonical)
-{?>
+function meta($pageTitle = '', $pageKeywords = '', $pageDescription = '', $pageCanonical = '')
+{ ?>
     <meta charset="utf-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-     <title><?php echo $pageTitle ?></title>
-     <meta name="keywords" content="<?php echo $pageKeywords ?>">
-     <meta name="description" content="<?php echo $pageDescription ?>">
-    <link rel="canonical" href="<?php echo $pageCanonical ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8'); ?></title>
+    <meta name="keywords" content="<?php echo htmlspecialchars($pageKeywords, ENT_QUOTES, 'UTF-8'); ?>">
+    <meta name="description" content="<?php echo htmlspecialchars($pageDescription, ENT_QUOTES, 'UTF-8'); ?>">
+    <link rel="canonical" href="<?php echo htmlspecialchars($pageCanonical, ENT_QUOTES, 'UTF-8'); ?>">
     <meta name="robots" content="index, follow">
 <?php
- }
- ?>
+}
+?>
  
